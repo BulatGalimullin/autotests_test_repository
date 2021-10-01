3d5bf6d5a97f200b7730ae002a4d1df4dcb7e7bc
bigalimullin <bulat-003@yandex.ru>
2021-10-01 10:30:36 +0300

    add changes to the main page files

---
+++ pages/main_page.py
@@ -0,0 +1,21 @@
+from .base_page import BasePage
+from .locators import MainPageLocators
+from selenium.common.exceptions import NoAlertPresentException
+
+
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+
+        try:
+            alert = self.browser.switch_to.alert
+            alert.accept()
+        except NoAlertPresentException:
+            print("No alert")
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+
+
+
