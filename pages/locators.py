1acf29fecf7224cd33ce5f2ddc6934e3bba63328
bigalimullin <bulat-003@yandex.ru>
2021-10-01 10:30:04 +0300

    add some tests for the product page

---
+++ pages/locators.py
@@ -0,0 +1,18 @@
+from selenium.webdriver.common.by import By
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.ID, "login_form")
+    REGISTER_FORM = (By.ID, "register_form")
+
+class ProductPageLocators():
+    ADD_TO_CARD_BUTTON = (By.CLASS_NAME, "btn-add-to-basket")
+    REVIEW_BUTTON = (By.ID, "write_review")
+    TOTAL_BASKET_PRICE = (By.CSS_SELECTOR, 'div.alertinner  > p > strong')
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.col-sm-6 > h1")
+    PRODUCT_NAME_MESSAGE = (By.CSS_SELECTOR, "div#messages > :nth-child(1) > div > strong")
+
---
+++ pages/product_page.py
@@ -0,0 +1,26 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def is_review_button_presented_in_page(self):
+        assert self.is_element_present(*ProductPageLocators.REVIEW_BUTTON), "There is no review button in the page"
+
+    def add_to_card(self):
+        card = self.browser.find_element(*ProductPageLocators.ADD_TO_CARD_BUTTON)
+        card.click()
+
+    def is_item_added_to_card(self):
+        self.is_basket_equal_to_item_price()
+        self.is_this_product_added_to_basket()
+
+    def is_basket_equal_to_item_price(self):
+        basket_price = self.browser.find_element(*ProductPageLocators.TOTAL_BASKET_PRICE).text
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        assert basket_price == product_price, "Price in basket is not equal to the product's price"
+
+    def is_this_product_added_to_basket(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        product_name_message = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_MESSAGE).text
+        assert product_name == product_name_message, "Name of the product in the page is not the same as in the notification message"
+
+
